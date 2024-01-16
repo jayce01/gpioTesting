b784cd36f73630ddd6f87d6a5d495072b0f1563b
jayce <jaycecorcoran@gmail.com>
2024-01-16 01:16:46 -0500

    gpioTesting for RPI3B+

--- buttonTest.cpp
+++ buttonTest.cpp
@@ -1,33 +1,15 @@
 #include "buttonTest.h"
 #include "pigpio.h"
 
-
 #define BUTTON_PIN 17 // GPIO pin connected to the button
 #define LED_PIN 18    // GPIO pin connected to the LED
+
 buttonTest::buttonTest() {}
 
-void buttonTest::run(){
-    buttonTest::testingFunction();
-}
-
-void buttonTest::buttonCallback(int gpio, int level, uint32_t tick) {
-    if (level == 0) { // Button is pressed (active low)
-        cout << "Button pressed! LED will start blinking." << endl;
-
-        // Start blinking the LED
-        gpioPWM(LED_PIN, 128); // 50% duty cycle
-    } else { // Button is released
-        cout << "Button released! LED will stop blinking." << endl;
-
-        // Stop the LED
-        gpioPWM(LED_PIN, 0);
-    }
-}
-
-int buttonTest::testingFunction(){
+void buttonTest::run() {
     if (gpioInitialise() < 0) {
         cerr << "pigpio initialization failed." << endl;
-        return 1;
+        return;
     }
 
     // Set up the button pin as an input with a pull-up resistor
@@ -37,15 +19,26 @@ int buttonTest::testingFunction(){
     // Set up the LED pin as an output
     gpioSetMode(LED_PIN, PI_OUTPUT);
 
-    // Set up a callback for button events
-    gpioSetAlertFunc(BUTTON_PIN, buttonTest::buttonCallback);
-
-    // Run the program until the user presses Enter
+    // Run the loop until the user presses Enter
     cout << "Press Enter to exit." << endl;
-    cin.get();
+    while (cin.get() != '\n') {
+        // Check the button status
+        int buttonStatus = gpioRead(BUTTON_PIN);
+
+        // Button is pressed (active low)
+        if (buttonStatus == PI_LOW) {
+            cout << "Button pressed! LED will start blinking." << endl;
+
+            // Start blinking the LED
+            gpioPWM(LED_PIN, 128); // 50% duty cycle
+        } else { // Button is released
+            cout << "Button released! LED will stop blinking." << endl;
+
+            // Stop the LED
+            gpioPWM(LED_PIN, 0);
+        }
+    }
 
     // Clean up
     gpioTerminate();
-
-    return 0;
 }
--- buttonTest.h
+++ buttonTest.h
@@ -8,11 +8,11 @@ using namespace std;
 class buttonTest {
 public:
     buttonTest();
-    int testingFunction();
+    //int testingFunction();
     void run();
 
 private:
-    static void buttonCallback(int gpio, int level, uint32_t tick); //gpioAlertFunc_t f
+    //static void buttonCallback(int gpio, int level, uint32_t tick); //gpioAlertFunc_t f
 };
 
 #endif //BUTTONTEST_H
