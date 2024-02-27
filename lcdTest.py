f6584c36626d272dc5e68c684de17495d1596d72
jayce <jaycecorcoran@gmail.com>
2024-02-27 18:21:10 -0500

    test

--- lcdTest.py
+++ lcdTest.py
@@ -3,9 +3,11 @@ from rpi_lcd import LCD
 
 lcd = LCD();
 
-def safe_exit(signum, frame)
+def safe_exit(signum, frame):
     exit(1)
 
+signal(SIGTERM, safe_exit)
+signal(SIGHUP, safe_exit)
 try:
     lcd.text("hello")
     pause()
