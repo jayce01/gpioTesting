04563fad2986e7909bafc59b62a00da908d524cb
jayce <jaycecorcoran@gmail.com>
2024-02-27 18:30:20 -0500

    test

--- lcdTest.py
+++ lcdTest.py
@@ -10,6 +10,9 @@ signal(SIGTERM, safe_exit)
 signal(SIGHUP, safe_exit)
 try:
     lcd.text("hello", 1)
+    lcd.text("Wow", 2)
+    lcd.text("He", 3)
+    lcd.text("Ow", 4)
     pause()
 
 except KeyboardInterrupt:
